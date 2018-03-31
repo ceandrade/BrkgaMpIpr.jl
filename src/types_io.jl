6e76cf3f822a5383b845a9e3c0286bf1aa8283ba
Carlos Eduardo de Andrade <ce.andrade@gmail.com>
2018-03-31 00:50:51 -0400

    Adding write_configuration() and tests.

--- src/types_io.jl
+++ src/types_io.jl
@@ -6,7 +6,7 @@
 # This code is released under LICENSE.md.
 #
 # Created on:  Mar 24, 2018 by ceandrade
-# Last update: Mar 24, 2018 by ceandrade
+# Last update: Mar 30, 2018 by ceandrade
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -50,4 +50,38 @@ function parse(::Type{BiasFunction}, value::String)::BiasFunction
     end
 
     throw(ArgumentError("cannot parse $value as BiasFunction"))
+end
+
+################################################################################
+
+"""
+"""
+function write_configuration(brkga_data::BrkgaData,
+                             external_params::ExternalControlParams,
+                             filename::String)
+
+    elite_percentage = brkga_data.elite_size / brkga_data.population_size;
+    mutants_percentage = brkga_data.num_mutants / brkga_data.population_size;
+
+    # TODO (ceandrade): implement the path relink parameters.
+    open(filename, "w") do file
+        write(file,
+              "population_size $(brkga_data.population_size)\n",
+              "elite_percentage $(elite_percentage)\n",
+              "mutants_percentage $(mutants_percentage)\n",
+              "mutants_percentage $(mutants_percentage)\n",
+              "elite_parents $(brkga_data.num_elite_parents)\n",
+              "total_parents $(brkga_data.total_parents)\n",
+              "bias_function $(brkga_data.bias)\n",
+              "independent_populations $(brkga_data.num_independent_populations)\n",
+              # "pr_minimum_distance $(brkga_data.pr_minimum_distance)\n",
+              # "pr_type $(brkga_data.pr_type)\n",
+              # "alpha_block_size $(brkga_data.alpha_block_size)\n",
+              # "pr_percentage $(brkga_data.pr_percentage)\n",
+              "exchange_interval $(external_params.exchange_interval)\n",
+              "num_exchange_indivuduals $(external_params.num_exchange_indivuduals)\n",
+              "reset_interval $(external_params.reset_interval)\n",
+        )
+    end
+    nothing
 end
--- test/types_io_tests.jl
+++ test/types_io_tests.jl
@@ -6,7 +6,7 @@
 # This code is released under LICENSE.md.
 #
 # Created on:  Mar 24, 2018 by ceandrade
-# Last update: Mar 27, 2018 by ceandrade
+# Last update: Mar 31, 2018 by ceandrade
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -30,4 +30,85 @@
     @test parse(BrkgaMpIpr.BiasFunction, "QUADRATIC") == QUADRATIC
     @test parse(BrkgaMpIpr.BiasFunction, "QuAdRaTiC") == QUADRATIC
     @test_throws ArgumentError parse(BrkgaMpIpr.BiasFunction, "invalid")
+end
+
+################################################################################
+
+@testset "write_configuration()" begin
+    #########################
+    # From config file
+    #########################
+
+    config_path = joinpath(@__DIR__, "configuration_files")
+
+    const local_param_values = [
+        default_param_values[param_index["instance"]],
+        default_param_values[param_index["decode!"]],
+        default_param_values[param_index["opt_sense"]],
+        default_param_values[param_index["seed"]],
+        default_param_values[param_index["chr_size"]]
+    ]
+
+    brkga_data, external_params =
+        build_brkga(local_param_values...,
+                    joinpath(config_path, "regular.conf"))
+
+    @test_throws SystemError write_configuration(brkga_data, external_params,
+                                                 "/invalid")
+
+    temp_filename = tempname()
+    write_configuration(brkga_data, external_params, temp_filename)
+
+    result = ""
+    open(temp_filename) do file
+        result = lowercase(readstring(file))
+    end
+    rm(temp_filename)
+
+    # TODO (ceandrade): add the path relink parameters.
+    standard = """
+population_size 500
+elite_percentage 0.3
+mutants_percentage 0.15
+mutants_percentage 0.15
+elite_parents 2
+total_parents 3
+bias_function loginverse
+independent_populations 3
+exchange_interval 200
+num_exchange_indivuduals 2
+reset_interval 600
+"""
+    @test result == standard
+
+    #########################
+    # From direct building
+    #########################
+    param_values = copy(default_param_values)
+    brkga_data = build_brkga(param_values...)
+
+    temp_filename = tempname()
+    write_configuration(brkga_data, ExternalControlParams(0, 0, 0),
+                        temp_filename)
+
+    result = ""
+    open(temp_filename) do file
+        result = lowercase(readstring(file))
+    end
+    rm(temp_filename)
+
+    standard = """
+population_size 10
+elite_percentage 0.3
+mutants_percentage 0.1
+mutants_percentage 0.1
+elite_parents 1
+total_parents 2
+bias_function loginverse
+independent_populations 3
+exchange_interval 0
+num_exchange_indivuduals 0
+reset_interval 0
+"""
+    @test result == standard
 end
